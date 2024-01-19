52277180a220440f440ec5a8e567c4c1ff23948e
Kiito Amemiya <2026amemiya.key@fcboe.org>
2024-01-19 15:17:57 -0500

    Finished Mario Project

--- mario.c
+++ mario.c
@@ -3,11 +3,11 @@
 int main()
 {
 	printf("       #\n");
-	printf("       #\n");
-	printf("       #\n");
-	printf("       #\n");
-	printf("       #\n");
-	printf("       #\n");
+	printf("      ##\n");
+	printf("     ###\n");
+	printf("    ####\n");
+	printf("   #####\n");
+	printf("  ######\n");
 	printf(" #######\n");
 	printf("########\n");
 	return 0;
